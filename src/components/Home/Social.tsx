e9b7f3984a707cf796bf4509b11016e8b6c8dc54
Zaid Chbani <zchbani@e2r2p14.1337.ma>
2023-05-27 17:31:48 +0100

    about page

---
+++ src/components/Home/Social.tsx
@@ -0,0 +1,32 @@
+const Social = () => {
+  return (
+    <div className="home_social">
+      <a
+        href="https://twitter.com/zaaay_d"
+        className="home_social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-twitter-alt"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/zaid-chbani-a81315227/"
+        className="home_social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-linkedin-alt"></i>
+      </a>
+      <a
+        href="https://github.com/zaayddev"
+        className="home_social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
