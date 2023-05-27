2ca96e9cd6e6f196048ad1f574423adcb460c8ae
Zaid Chbani <zchbani@e2r3p6.1337.ma>
2023-05-27 22:50:04 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
-import About from "./components/About/About"
+import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
+import Services from "./components/Services/Services";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Home />
         <About />
         <Skills />
+        <Services />
       </main>
     </>
   );
---
+++ src/components/Services/Services.tsx
@@ -0,0 +1,137 @@
+import "./services.css";
+
+const Services = () => {
+  return (
+    <section className="services section" id="services">
+      <h2 className="section_title">Services</h2>
+      <span className="section_subtitle">What I offer</span>
+
+      <div className="services_container container grid">
+        <div className="services_content">
+          <div>
+            <i className="uil uil-web-grid services_icon"></i>
+            <h3 className="services_title"></h3>
+          </div>
+          <span className="serivces_button">
+            View More{" "}
+            <i className="uil uil-arrow-right services_button-icon"></i>
+            <div className="services_modal">
+              <div className="services_modal-content">
+                <i className="uil uil-times services_modal-close"></i>
+                <h3 className="services_modal-title"></h3>
+                <p className="services_modal-description">
+                  djhfjadhfald afkhdfadfkfhjhf fdhldahfehoaw agj gaweh'aeghge
+                  gfdah
+                </p>
+                <ul className="services_modal-services grid">
+                  <li className="services_modal-service">
+                    <i className="uil uil-check-circle services_modal-icon"></i>
+                    <p className="services_modal-info">
+                      I develop the user interface.
+                    </p>
+                  </li>
+
+                  <li className="services_modal-service">
+                    <i className="uil uil-check-circle services_modal-icon"></i>
+                    <p className="services_modal-info">Web page development.</p>
+                  </li>
+
+                  <li className="services_modal-service">
+                    <i className="uil uil-check-circle services_modal-icon"></i>
+                    <p className="services_modal-info">
+                      I develop the user interface.
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </span>
+        </div>
+
+        <div className="services_content">
+          <div>
+            <i className="uil uil-arrow services_icon"></i>
+            <h3 className="services_title"></h3>
+          </div>
+          <span className="serivces_button">
+            View More{" "}
+            <i className="uil uil-arrow-right services_button-icon"></i>
+            <div className="services_modal">
+              <div className="services_modal-content">
+                <i className="uil uil-times services_modal-close"></i>
+                <h3 className="services_modal-title"></h3>
+                <p className="services_modal-description">
+                  djhfjadhfald afkhdfadfkfhjhf fdhldahfehoaw agj gaweh'aeghge
+                  gfdah
+                </p>
+                <ul className="services_modal-services grid">
+                  <li className="services_modal-service">
+                    <i className="uil uil-check-circle services_modal-icon"></i>
+                    <p className="services_modal-info">
+                      I develop the user interface.
+                    </p>
+                  </li>
+
+                  <li className="services_modal-service">
+                    <i className="uil uil-check-circle services_modal-icon"></i>
+                    <p className="services_modal-info">Web page development.</p>
+                  </li>
+
+                  <li className="services_modal-service">
+                    <i className="uil uil-check-circle services_modal-icon"></i>
+                    <p className="services_modal-info">
+                      I develop the user interface.
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </span>
+        </div>
+
+        <div className="services_content">
+          <div>
+            <i className="uil uil-edit services_icon"></i>
+            <h3 className="services_title"></h3>
+          </div>
+          <span className="serivces_button">
+            View More{" "}
+            <i className="uil uil-arrow-right services_button-icon"></i>
+            <div className="services_modal">
+              <div className="services_modal-content">
+                <i className="uil uil-times services_modal-close"></i>
+                <h3 className="services_modal-title"></h3>
+                <p className="services_modal-description">
+                  djhfjadhfald afkhdfadfkfhjhf fdhldahfehoaw agj gaweh'aeghge
+                  gfdah
+                </p>
+                <ul className="services_modal-services grid">
+                  <li className="services_modal-service">
+                    <i className="uil uil-check-circle services_modal-icon"></i>
+                    <p className="services_modal-info">
+                      I develop the user interface.
+                    </p>
+                  </li>
+
+                  <li className="services_modal-service">
+                    <i className="uil uil-check-circle services_modal-icon"></i>
+                    <p className="services_modal-info">Web page development.</p>
+                  </li>
+
+                  <li className="services_modal-service">
+                    <i className="uil uil-check-circle services_modal-icon"></i>
+                    <p className="services_modal-info">
+                      I develop the user interface.
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
