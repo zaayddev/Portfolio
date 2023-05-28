6ee190ae8b640b2b5978743710792bdf670c44be
zaaydx <zaid.chbani@usmba.ac.ma>
2023-05-28 23:37:03 +0100

    qualification sections

--- src/components/Qualification/Qualification.tsx
+++ src/components/Qualification/Qualification.tsx
@@ -1,11 +1,194 @@
-import React from "react";
+import React, { useState } from "react";
 import "./qualification.css";
 
 function Qualification() {
+  const [toggleSate, setToggleState] = useState<number>(0);
+
+  const toggleTab = (index: number) => {
+    setToggleState(index);
+  };
   return (
     <section className="qualification section" id="services">
       <h2 className="section_title">Qualification</h2>
       <span className="section_subtitle">My personel journey</span>
+
+      <div className="qualification_container container">
+        <div className="qualification_tabs">
+          <div
+            className={
+              toggleSate === 1
+                ? "qualification_button qualification_active button--flex"
+                : "qualification_button button--flex"
+            }
+            onClick={() => toggleTab(1)}
+          >
+            <i className="uil uil-graduation-cap  qualification_icon"></i>
+            Education
+          </div>
+
+          <div
+            className={
+              toggleSate === 2
+                ? "qualification_button qualification_active button--flex"
+                : "qualification_button button--flex"
+            }
+            onClick={() => toggleTab(2)}
+          >
+            <i className="uil uil-briefcase-alt qualification_icon"></i>
+            Experience
+          </div>
+        </div>
+
+        <div className="qualification_sections">
+          <div
+            className={
+              toggleSate === 1
+                ? "qualification_content qualification_content-active"
+                : "qualification_content"
+            }
+          >
+            <div className="qualification_data">
+              <div>
+                <h3 className="qualification_title">Web Design</h3>
+                <span className="qualification_subtitle">1337 School</span>
+                <div className="qualification_calendar">
+                  <i className="uil uil-calendar-alt"></i>
+                  2021 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification_rounder"></span>
+                <span className="qualification_line"></span>
+              </div>
+            </div>
+
+            <div className="qualification_data">
+              <div></div>
+
+              <div>
+                <span className="qualification_rounder"></span>
+                <span className="qualification_line"></span>
+              </div>
+              <div>
+                <h3 className="qualification_title">Frontend developer</h3>
+                <span className="qualification_subtitle">1337 School</span>
+                <div className="qualification_calendar">
+                  <i className="uil uil-calendar-alt"></i>
+                  2021 - Present
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification_data">
+              <div>
+                <h3 className="qualification_title">Web Developer</h3>
+                <span className="qualification_subtitle">1337 School</span>
+                <div className="qualification_calendar">
+                  <i className="uil uil-calendar-alt"></i>
+                  2021 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification_rounder"></span>
+                <span className="qualification_line"></span>
+              </div>
+            </div>
+
+            <div className="qualification_data">
+              <div></div>
+
+              <div>
+                <span className="qualification_rounder"></span>
+                <span className="qualification_line"></span>
+              </div>
+              <div>
+                <h3 className="qualification_title">Frontend developer</h3>
+                <span className="qualification_subtitle">1337 School</span>
+                <div className="qualification_calendar">
+                  <i className="uil uil-calendar-alt"></i>
+                  2021 - Present
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className={
+              toggleSate === 2
+                ? "qualification_content qualification_content-active"
+                : "qualification_content"
+            }
+          >
+            <div className="qualification_data">
+              <div>
+                <h3 className="qualification_title">Peb Design</h3>
+                <span className="qualification_subtitle">1337 School</span>
+                <div className="qualification_calendar">
+                  <i className="uil uil-calendar-alt"></i>
+                  2021 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification_rounder"></span>
+                <span className="qualification_line"></span>
+              </div>
+            </div>
+
+            <div className="qualification_data">
+              <div></div>
+
+              <div>
+                <span className="qualification_rounder"></span>
+                <span className="qualification_line"></span>
+              </div>
+              <div>
+                <h3 className="qualification_title">Frontend developer</h3>
+                <span className="qualification_subtitle">1337 School</span>
+                <div className="qualification_calendar">
+                  <i className="uil uil-calendar-alt"></i>
+                  2021 - Present
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification_data">
+              <div>
+                <h3 className="qualification_title">Web Developer</h3>
+                <span className="qualification_subtitle">1337 School</span>
+                <div className="qualification_calendar">
+                  <i className="uil uil-calendar-alt"></i>
+                  2021 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification_rounder"></span>
+                <span className="qualification_line"></span>
+              </div>
+            </div>
+
+            <div className="qualification_data">
+              <div></div>
+
+              <div>
+                <span className="qualification_rounder"></span>
+                <span className="qualification_line"></span>
+              </div>
+              <div>
+                <h3 className="qualification_title">Frontend developer</h3>
+                <span className="qualification_subtitle">1337 School</span>
+                <div className="qualification_calendar">
+                  <i className="uil uil-calendar-alt"></i>
+                  2021 - Present
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
